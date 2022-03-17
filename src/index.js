9e3c060f679f964f30c702bf20511d1722e9b1a3
rev <dasdev.pratik@gmail.com>
2022-03-17 18:12:32 +0530

    moving the store to another file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 // import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './store';
 import { Provider } from 'react-redux';
-import filterReducer from './reducer/filterReducer';
-import { configureStore } from '@reduxjs/toolkit';
-import noteService from './services/notes'                    
-import noteReducer,{ setNotes } from './reducer/noteReducer';
   
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter:filterReducer
-  }
-})
-
-noteService.getAll().then(notes => 
-    store.dispatch(setNotes(notes))
-)
 
 ReactDOM.render(
   <Provider store={store}>
